5d6d181e653beb428cd445cc6eb3766dabdeb592
BelArtCom <belartcom2009@gmail.com>
2021-07-07 20:53:45 +1000

    Clean2

--- code.js
+++ code.js
@@ -1,13 +1,21 @@
 var word = "bottles";
 var count = 99;
-while (count > 0) {
-	console.log(count + " " + word + " of beer on the wall");
-	console.log(count + " " + word + " of beer,");
-	console.log("Take one down, pass it around,");
-	count = count - 1;
-	if (count > 0) {
-		console.log(count + " " + word + " of beer on the wall.");
-	} else {
-		console.log("No more " + word + " of beer on the wall.");
+
+fuction first_function (f_str, f_count) {
+	var f_output;
+	var kar = "a\nb";
+	while (f_count > 0) {
+		f_output = f_output + f_count + " " + word + " of beer on the wall" + kar;
+		f_output = f_output + f_count + " " + word + " of beer," + kar;
+		f_output = f_output + "Take one down, pass it around," + kar;
+		f_count = f_count - 1;
+		if (f_count > 0) {
+			f_output = f_output + f_count + " " + word + " of beer on the wall." + kar;
+		} else {
+			f_output = f_output + "No more " + word + " of beer on the wall." + kar;
+		}
 	}
+	return f_output;
 }
+
+console.log(first_function);
