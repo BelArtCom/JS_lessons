99922c9a83a2a27bf65cb7bbab80450772bc4108
Artur Beliyanskiy <belartcom2009@gmail.com>
2021-09-27 15:59:50 +0300

    0-base cell index -> 1 base cell indes + fix hit handling

--- battleship.js
+++ battleship.js
@@ -1,41 +1,44 @@
 const SEA_CELLS_NUMBER = 10;
 const SHIP_CELLS_NUMBER = 5;
 
-const maxCellIndex = SEA_CELLS_NUMBER - 1;
-const start = Math.floor(
-  Math.random() * (SEA_CELLS_NUMBER - SHIP_CELLS_NUMBER + 1)
-);
+const start =
+  Math.floor(Math.random() * (SEA_CELLS_NUMBER - SHIP_CELLS_NUMBER + 1)) + 1;
+
+console.log("start = " + start);
 
 const locationList = Array.from(
   { length: SHIP_CELLS_NUMBER },
   (_, i) => i + start
 );
-
+let guessList = [];
 let hits = 0;
 let guesses = 0;
 let isSunk = false;
 
 while (!isSunk) {
   const guess = Number(
-    prompt(`Ready, aim, fire! (enter a number 0-${maxCellIndex}):`)
+    prompt(`Готовься, целься, стреляй! (введите число 1-${SEA_CELLS_NUMBER}):`)
   );
-  if (Number.isNaN(guess) || guess < 0 || guess > maxCellIndex) {
-    alert("Please enter a valid cell number!");
+  if (Number.isNaN(guess) || guess < 1 || guess > SEA_CELLS_NUMBER) {
+    alert("Пожалуйста, введите корректный номер ячейки!");
   } else {
     guesses++;
     if (locationList.includes(guess)) {
-      hits++;
-      alert("HIT!");
+      if (!guessList.includes(guess)) {
+        hits++;
+      }
+      guessList.push(guess);
+      alert("ПОПАДАНИЕ!");
       if (hits == SHIP_CELLS_NUMBER) {
         isSunk = true;
-        alert("You sank my battleship!");
-        const accuracy = SHIP_CELLS_NUMBER / guesses;
+        alert("Корабль потоплен!");
+        const accuracy = Math.round((SHIP_CELLS_NUMBER / guesses) * 100);
         alert(
-          `You took ${guesses} guesses to sink the battleship, which means your shooting accuracy was ${accuracy}`
+          `Совершено выстрелов: ${guesses}. Точность стрельбы: ${accuracy} %`
         );
       }
     } else {
-      alert("MISS");
+      alert("ПРОМАХ");
     }
   }
 }
